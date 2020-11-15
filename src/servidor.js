14a0c28937c8b170866eb53146e373499ca21205
Anderson Rocha <usuariodeanderson@gmail.com>
2020-11-14 22:51:41 -0300

    FirstCommit

---
+++ src/servidor.js
@@ -0,0 +1,44 @@
+const porta = 3003
+
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({extended: true }))
+
+app.get('/produtos', (req, res, next) => {
+    res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res , next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req , res , next) => {
+   const produto = bancoDeDados.salvarProduto({
+       nome: req.body.nome,
+       preco: req.body.preco
+   }) 
+   res.send(produto)
+})
+
+app.put('/produtos', (req , res , next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.body.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    }) 
+    res.send(produto)
+ })
+
+ app.delete('/produtos/:id', (req , res , next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id) 
+    res.send(produtos)
+ })
+
+
+app.listen(porta, () => {
+    console.log(`Servidor executanto na porta ${porta}`)
+})
